5ed9df061a4d27a208e5ead32cb7128932dfa566
blueG123 <inakalajoie@gmail.com>
2025-06-22 12:12:33 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,24 @@ window.addEventListener("DOMContentLoaded", () => {
       contenu.style.transform = "translateY(0)";
     }, 200);
   }
+
+  // Gestion du menu responsive mobile
+  const btnMenu = document.getElementById("btn-menu");
+  const navbarCache = document.getElementById("navbar-cache");
+  const navbarClicable = document.getElementById("navbar-clicable");
+
+  if (btnMenu && navbarCache) {
+    btnMenu.addEventListener("click", () => {
+      navbarCache.style.display = "block";
+      document.body.style.overflow = "hidden";
+    });
+  }
+  if (navbarClicable && navbarCache) {
+    navbarClicable.addEventListener("click", () => {
+      navbarCache.style.display = "none";
+      document.body.style.overflow = "auto";
+    });
+  }
 });
 
 document.querySelectorAll("h2, h3").forEach((titre) => {
