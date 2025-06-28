c0e9f984e0f17581e43705c2f61c12f2024f7592
blueG123 <inakalajoie@gmail.com>
2025-06-28 11:51:55 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
-  // Correction du sélecteur pour cibler la section "A propos"
-  const contenu = document.querySelector("#apropos");
-  if (contenu) {
-    contenu.style.opacity = 0;
-    contenu.style.transform = "translateY(30px)";
-    contenu.style.transition = "all 1s ease";
-    setTimeout(() => {
-      contenu.style.opacity = 1;
-      contenu.style.transform = "translateY(0)";
-    }, 200);
-  }
+
 
   // Gestion du menu responsive mobile
   const btnMenu = document.getElementById("btn-menu");
@@ -28,7 +17,5 @@ window.addEventListener("DOMContentLoaded", () => {
       document.body.style.overflow = "auto";
     });
   }
-});
 
-// Gestion de la fermeture du menu responsive mobile en cliquant à l'extérieur
-window.addEventListener("click", (event) => { 
+
